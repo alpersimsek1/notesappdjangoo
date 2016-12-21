ad042f34b2b31aa4f00a1dd04011352684b52f83
Alper Simsek <alpersim94@gmail.com>
2016-12-21 16:43:06 +0300

    new branh commit

--- notes/forms.py
+++ notes/forms.py
@@ -1,7 +1,32 @@
-from django.forms import ModelForm
-from .models import Movie
+from django.forms import ModelForm, Textarea, URLField, SelectDateWidget, SplitHiddenDateTimeWidget
+from django.forms.extras.widgets import SelectDateWidget
+from .models import Movie, Note, Calendar
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
 
 class MovieForm(ModelForm):
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['name', 'release_date', 'imdb_link', 'categories']
+        widgets = {
+            'release_date': SelectDateWidget,
+        }
+
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'body', 'created_at', 'categories']
+        widgets = {
+            'comment': Textarea(attrs={'cols': 40, 'rows': 15}),
+
+        }
+
+class CalendarForm(ModelForm):
+    class Meta:
+        model = Calendar
+        fields = ['title', 'date_when']
+        widgets = {
+            'date_when': SelectDateWidget,
+        }
+
--- notes/views.py
+++ notes/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Movie, Note, News, Calendar, Category
 from django.views.generic import View
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from .forms import NoteForm, MovieForm, CalendarForm
+
 
 class MovieList(ListView):
     template_name = 'notes/movie_list.html'
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.order_by('release_date')
     context_object_name = 'all_movies'
 
 class NoteList(ListView):
@@ -15,32 +18,49 @@ class NoteList(ListView):
     queryset = Note.objects.all()
     context_object_name = 'all_notes'
 
+
+
 class NewsList(ListView):
     model = News
 
 class CalendarList(ListView):
     model = Calendar
-
+    queryset = Calendar.objects.order_by("date_when")
+    context_object_name = 'all_calendar'
 
 class IndexView(ListView):
     template_name = 'notes/index.html'
     context_object_name = 'all_movies'
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.order_by('release_date')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['notes'] = Note.objects.all()
-        context['calendar'] = Calendar.objects.all()
+        context['all_notes'] = Note.objects.all()
+        context['all_calendar'] = Calendar.objects.all()
         # And so on for more models
         return context
 
-class MovieCreate(CreateView):
-    model = Movie
-    fields = ['name', 'release_date', 'imdb_link']
+
+class NoteCreate(CreateView):
+    form_class = NoteForm
+    template_name = 'notes/note_form.html'
     success_url = reverse_lazy('notes:index')
 
 
-class NoteCreate(CreateView):
-    model = Note
-    fields = ['title', 'body', 'created_at']
+class MovieCreate(CreateView):
+    form_class = MovieForm
+    template_name = 'notes/movie_form.html'
+    success_url = reverse_lazy('notes:index')
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    fields = ['name', 'release_date', 'imdb_link', 'categories']
+
+class MovieDelete(DeleteView):
+    model = Movie
+    success_url = reverse_lazy('notes:index')
+
+class CalendarCreate(CreateView):
+    form_class = CalendarForm
+    template_name = 'notes/calendar_form.html'
     success_url = reverse_lazy('notes:index')
